cfe25a422a9cd3a17ec9cd965589310998ed1c59
Adela Darmansyah <adela.yohana@gmail.com>
2020-08-17 13:31:26 -0700

    Move shared state from Square to Board component, update click event handler

--- src/index.js
+++ src/index.js
@@ -4,30 +4,41 @@ import './index.css';
 
 // The Square component renders a single <button>
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        // In JavaScript, the super keyword is used to access and call functions on an object's parent. All React component classes that have a constructor should start with a super(props) call as we are defining the constructor of a SUBCLASS.
-        this.state = {
-            value: null,
-        };
-    }
-
     render() {
         return (
             <button 
                 className="square" 
-                onClick={() => this.setState({value: 'X'})}
-            >
-            {this.state.value}
+                onClick={() => this.props.onClick()}
+                
+            >             
+                {this.props.value}
             </button>
         );
     }
 }
 
 // The Board component renders 9 Squares
+// The Square components are now called controlled components. The Board has full control over them.
 class Board extends React.Component {
+    // In JavaScript, the super keyword is used to access and call functions on an object's parent. All React component classes that have a constructor should start with a super(props) call as we are defining the constructor of a SUBCLASS.
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice(); // creates a copy of the squares array for immutability
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square 
+            value={this.state.squares[i]} 
+            onClick={() => this.handleClick(i)}
+        />;
     }
   
     render() {
